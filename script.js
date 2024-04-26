7125413482c32f0f663417a5a1089053b35cc038
InsertDisplayHere <168029043+InsertDisplayHere@users.noreply.github.com>
2024-04-26 23:13:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//Letsdo this!
+function calculateTotal()  {
+		var teacherCount =
+	parseInt(document.getElementById("teacherCount").value);
+		var studentCount =
+	parseInt(document.getElementById)("studentCount").value};
+	
+//Big space
+	
+	var totalPeople = teacherCount + studentCount;
+	
+	
+//Big space2
+				
+				
+		document.getElementById("result").textContent = "Toplam Kisi sayisi: " + totalPeople;
+}
